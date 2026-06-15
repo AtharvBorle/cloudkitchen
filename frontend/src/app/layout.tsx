1a06c624adac5833ea018077fc40b56f260f89f8
Atharv Borle <atharvabbvd@gmail.com>
2026-06-15 16:14:56 +0530

    seller sidebar padding

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <CartProvider>
-            <main className="container">{children}</main>
+            <main>{children}</main>
           </CartProvider>
         </Providers>
       </body>
