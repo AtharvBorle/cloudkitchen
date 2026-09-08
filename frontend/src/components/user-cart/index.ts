f298712e7504645a20f509465a02eec02c7b40ee
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2026-09-08 18:42:51 +0530

    added user-cart and user-checkout Desktop UI pages

---
+++ frontend/src/components/user-cart/index.ts
@@ -0,0 +1,8 @@
+export { default as UserCart, default } from "./UserCart";
+export type { UserCartProps, UserCartItem } from "./UserCart";
+
+export { default as UserCheckout } from "./UserCart";
+export type { UserCartProps as UserCheckoutProps } from "./UserCart";
+
+export { default as SecureCheckout } from "./SecureCheckout";
+export type { SecureCheckoutProps, CheckoutSummaryItem } from "./SecureCheckout";
